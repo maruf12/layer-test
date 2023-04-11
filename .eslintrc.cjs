4a83a855af3280ab4bd7d1693c8e2e34e68934f9
maruf12 <maruf.harsono@gmail.com>
2023-04-11 09:56:35 +0700

    add lint fix script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
+  extends: ['@nuxt/eslint-config'],
   rules: {
-    "vue/multi-word-component-names": ["error", {
-      "ignores": ["index"]
+    'vue/multi-word-component-names': ['error', {
+      'ignores': ['index']
     }]
   },
 };
